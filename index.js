3787e5e6f05a6855d286191ae77359b2b7248d3c
Aman Raj <amanraj3567@gmail.com>
2025-02-18 14:09:36 +0530

    Server 1.1.0

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = 3000;
 app.use(express.static(path.join(__dirname, "Public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "Public", "index2.html"));
+  res.sendFile(path.join(__dirname, "Public", "index.html"));
 });
 
 app.listen(PORT, () => {
