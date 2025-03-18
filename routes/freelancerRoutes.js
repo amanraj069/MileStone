a4151ada5dbd32577afeb4bea6138eaf347875cd
Vanya <iamvanyaawasthi@gmail.com>
2025-03-18 09:19:01 +0530

    fixed routes and changed logos

--- routes/freelancerRoutes.js
+++ routes/freelancerRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.route("/profile").get(freelancerController.getFreelancerProfile);
 router.route("/active_job").get(freelancerController.getFreelancerActiveJobs);
 router.route("/job_history").get(freelancerController.getFreelancerJobHistory);
-router.route("/job_history/see_more").get(freelancerController.getSeemore);
+router.route("/job_history/jhistory_see_more").get(freelancerController.getSeemore);
 router.route("/payment").get(freelancerController.getFreelancerPayment);
 router.route("/skills_badges").get(freelancerController.getFreelancerSkills);
 router
