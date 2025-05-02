73acd5f2ab585510259242c193235b1064051bf4
Aman Raj <amanraj3567@gmail.com>
2025-05-02 16:45:01 +0530

    Employer made Static

--- controllers/employerController.js
+++ controllers/employerController.js
@@ -1,184 +1,51 @@
-const path = require("path");
-const fs = require("fs").promises;
-
-const getProfileData = async () => {
-  const filePath = path.join(__dirname, "../data/EmployerD/profile.json");
-  const data = await fs.readFile(filePath, "utf8");
-  return JSON.parse(data);
-};
-
-const getTransactionData = async () => {
-  const filePath = path.join(__dirname, "../data/EmployerD/transaction.json");
-  const data = await fs.readFile(filePath, "utf8");
-  return JSON.parse(data);
-};
-
-const getJobsData = async () => {
-  const filePath = path.join(__dirname, "../data/EmployerD/jobs.json");
-  const data = await fs.readFile(filePath, "utf8");
-  return JSON.parse(data);
-};
-
-const getCurrentPreviousData = async () => {
-  const filePath = path.join(
-    __dirname,
-    "../data/EmployerD/current_previous.json"
-  );
-  const data = await fs.readFile(filePath, "utf8");
-  return JSON.parse(data);
-};
-
-exports.getEmployerProfile = async (req, res) => {
-  try {
-    const profileData = await getProfileData();
-    res.render("Abhishek/profile", { 
-      data: profileData,
-      user: req.session.user,
-      activePage: 'profile'
+const employerController = {
+  getCurrentJobs: (req, res) => {
+    res.render('Abhishek/current_jobs', {
+      user: { name: 'TechCorp Solutions' },
+      activePage: 'current_jobs'
     });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Server Error");
-  }
-};
+  },
 
-exports.getJobListings = async (req, res) => {
-  try {
-    const jobsData = await getJobsData();
-    res.render("Abhishek/job_listing", { 
-      jobs: jobsData.jobs,
-      user: req.session.user,
+  getJobListings: (req, res) => {
+    res.render('Abhishek/job_listing', {
+      user: { name: 'TechCorp Solutions' },
       activePage: 'job_listings'
     });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Server Error");
-  }
-};
+  },
 
-exports.getCurrentJobs = async (req, res) => {
-  try {
-    const data = await getCurrentPreviousData();
-    res.render("Abhishek/current_jobs", {
-      current_freelancers: data.current_freelancers,
-      user: req.session.user,
-      activePage: 'current_jobs'
+  getProfile: (req, res) => {
+    res.render('Abhishek/profile', {
+      user: { name: 'TechCorp Solutions' },
+      activePage: 'profile'
     });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Server Error");
-  }
-};
+  },
 
-exports.getPreviouslyWorked = async (req, res) => {
-  try {
-    const data = await getCurrentPreviousData();
-    res.render("Abhishek/previously_worked", {
-      previous_freelancers: data.previous_freelancers,
-      user: req.session.user,
+  getTransactionHistory: (req, res) => {
+    res.render('Abhishek/transaction', {
+      user: { name: 'TechCorp Solutions' },
+      activePage: 'transaction_history'
+    });
+  },
+
+  getMilestones: (req, res) => {
+    res.render('Abhishek/milestone', {
+      user: { name: 'TechCorp Solutions' },
+      activePage: 'transaction_history'
+    });
+  },
+
+  getPreviouslyWorked: (req, res) => {
+    res.render('Abhishek/previously_worked', {
+      user: { name: 'TechCorp Solutions' },
       activePage: 'previously_worked'
     });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Server Error");
-  }
-};
-
-exports.getTransactionHistory = async (req, res) => {
-  try {
-    const transactionData = await getTransactionData();
-    res.render("Abhishek/transaction", {
-      transactions: transactionData.transactions,
-      user: req.session.user,
-      activePage: 'transaction_history'
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Server Error");
-  }
-};
-
-exports.getSubscription = async (req, res) => {
-  try {
-    const profileData = await getProfileData();
-    res.render("Abhishek/subscription", { 
-      data: profileData,
-      user: req.session.user,
+  },
+  getSubscription: (req, res) => {
+    res.render('Abhishek/subscription', {
+      user: { name: 'TechCorp Solutions' },
       activePage: 'subscription'
     });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Server Error");
   }
 };
 
-exports.getChatsCurrentJobs = async (req, res) => {
-  try {
-    res.render("Abhishek/Additional/chat", {
-      user: req.session.user,
-      activePage: 'current_jobs'
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Server Error");
-  }
-};
-
-exports.getSeemoreCurrentJobs = (req, res) => {
-  res.sendFile(
-    path.join(
-      __dirname,
-      "../views/Abhishek/Additional/current_job_see_more.html"
-    )
-  );
-};
-
-exports.geSeemoreJoblistings = async (req, res) => {
-  try {
-    const jobsData = await getJobsData();
-    const jobId = req.params.jobId;
-    const job = jobsData.jobs.find((j) => j.id === jobId);
-    if (!job) {
-      return res.status(404).send("Job not found");
-    }
-    res.render("Abhishek/Additional/see_more_detail", { job });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Server Error");
-  }
-};
-
-exports.getmilestoneTransactionHistory = async (req, res) => {
-  try {
-    const transactionData = await getTransactionData();
-    const transactionId = req.params.txnId;
-    const transaction = transactionData.transactions.find(
-      (t) => t.id === transactionId
-    );
-    if (!transaction) {
-      return res.status(404).send("Transaction not found");
-    }
-    res.render("Abhishek/Additional/milestone", { transaction });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Server Error");
-  }
-};
-
-exports.getmilestoneCurrentjob = async (req, res) => {
-  try {
-    res.render("Abhishek/milestone", {
-      user: req.session.user,
-      activePage: 'transaction_history'
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Server Error");
-  }
-};
-
-exports.getDetailsofAppliers = (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "../views/Abhishek/Additional/view_profile.html")
-  );
-};
+module.exports = employerController;
--- routes/employerRoutes.js
+++ routes/employerRoutes.js
@@ -1,38 +1,14 @@
-const express = require("express");
-const employerController = require("../controllers/employerController");
-
+const express = require('express');
 const router = express.Router();
+const employerController = require('../controllers/employerController');
 
-router.route("/").get(employerController.getEmployerProfile);
-router.route("/profile").get(employerController.getEmployerProfile);
+router.get('/current-jobs', employerController.getCurrentJobs);
+router.get('/job-listings', employerController.getJobListings);
+router.get('/profile', employerController.getProfile);
+router.get('/transaction-history', employerController.getTransactionHistory);
+router.get('/milestones', employerController.getMilestones);
+router.get('/previously-worked', employerController.getPreviouslyWorked);
+router.get('/subscription', employerController.getSubscription);
 
-router.route("/job-listings").get(employerController.getJobListings);
-router
-  .route("/job-listings/see_more/:jobId")
-  .get(employerController.geSeemoreJoblistings);
-router
-  .route("/job-listings/see_more/job/view_profile")
-  .get(employerController.getDetailsofAppliers);
-
-router.route("/current-jobs").get(employerController.getCurrentJobs);
-router.route("/current-jobs/chat").get(employerController.getChatsCurrentJobs);
-router
-  .route("/current-jobs/see_more")
-  .get(employerController.getSeemoreCurrentJobs);
-
-router
-  .route("/current-jobs/see_more/milestone")
-  .get(employerController.getmilestoneCurrentjob);
-
-router.route("/previously-worked").get(employerController.getPreviouslyWorked);
-
-router
-  .route("/transaction-history")
-  .get(employerController.getTransactionHistory);
-router
-  .route("/transaction-history/milestone/:txnId")
-  .get(employerController.getmilestoneTransactionHistory);
-
-router.route("/subscription").get(employerController.getSubscription);
 
 module.exports = router;
