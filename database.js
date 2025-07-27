bc74e4d826aba3eca72d761d33d7ad3275981d4b
Aman Raj <amanraj3567@gmail.com>
2025-07-27 18:41:55 +0530

    Switched to Atlas

--- database.js
+++ database.js
@@ -1,18 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
-// MongoDB connection string
-const connectionString = 'mongodb://127.0.0.1:27017/Milestone';
+// MongoDB connection string from .env
+const connectionString = process.env.MONGODB_URI;
 
 // Connect to MongoDB
-mongoose.connect(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
-    console.log('Connected to MongoDB successfully');
+    console.log("Connected to MongoDB successfully");
   })
   .catch((err) => {
-    console.error('Error connecting to MongoDB:', err);
+    console.error("Error connecting to MongoDB:", err);
   });
 
 // Export the mongoose instance
